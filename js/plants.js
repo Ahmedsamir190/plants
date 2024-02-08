71e1b6d3e00598ce8ff181a7a6d81abad075d249
Ahmed samir <109394049+Ahmedsamir190@users.noreply.github.com>
2024-02-08 11:00:18 +0200

    Update plants.js

--- js/plants.js
+++ js/plants.js
@@ -34,9 +34,11 @@ let counter = setInterval(function () {
     let minutes = Math.floor((diffdate % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((diffdate % (1000 * 60)) / 1000);
 
-    document.querySelector(".days").innerHTML = days;
-    document.querySelector(".hours").innerHTML = hours;
-    document.querySelector(".minutes").innerHTML = minutes;
+ document.querySelector(".days").innerHTML = days < 10 ? `0${days}` : days;
+    document.querySelector(".hours").innerHTML =
+      hours < 10 ? `0${hours}` : hours;
+    document.querySelector(".minutes").innerHTML =
+      minutes < 10 ? `0${minutes}` : minutes;
     document.querySelector(".seconds").innerHTML =
       seconds < 10 ? `0${seconds}` : seconds;
   }
